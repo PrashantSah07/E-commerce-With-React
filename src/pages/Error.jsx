0615b8102117125a965f28b408249daf6787269e
prashant <prashantkumar855114@gmail.com>
2025-04-02 16:27:34 +0530

    add link attri in error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
       <h1 className="text-6xl font-bold">404</h1>
       <p className="text-xl mt-4">Oops! Page not found.</p>
-      <a href="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-        Go Home
-      </a>
-    </div>
+      <Link to='/' className='mt-6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600'>Go Home</Link>
+    </div >
   );
 };
 
